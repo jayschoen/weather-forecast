b556937e53b122eac359f89e45c6610738cfabe7
Jay Schoen <jay.r.schoen@gmail.com>
2022-02-17 16:59:41 -0500

    implement logic to take user input (address) and request data to retrieve forecast

--- weather-forecast/src/app/convert-address/convert-address.component.ts
+++ weather-forecast/src/app/convert-address/convert-address.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+
+import { WeatherService } from '../services/weather.service';
+
+import { Address } from '../address';
 
 @Component({
   selector: 'convert-address',
@@ -7,15 +13,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConvertAddressComponent implements OnInit {
 
-  street: string = '';
-  city: string = '';
-  state: string = '';
-  zip: string = '';
+  address: Address = {
+    street: '2140 Newport Place NW',
+    city: 'Washington',
+    state: 'DC',
+    zip: '20037'
+  };
 
-  constructor() { }
+  constructor(
+    private weatherService: WeatherService
+  ) { }
 
   ngOnInit(): void {
 
+    this.weatherService.lat_long.pipe(
+
+      mergeMap(
+        data => {
+          return this.grid(data);
+        }),
+      mergeMap(
+        data => {
+
+          let station = data['properties']['gridId'];
+          let gridX = data['properties']['gridX'];
+          let gridY = data['properties']['gridY'];
+          let coords = [gridX, gridY];
+
+          return this.forecast(station, coords);
+        }
+      )  
+    ).subscribe(
+      data => {
+        console.log(data);
+      },
+      error => console.error(error)
+    );
   }
 
+  addressToLatLong(address: Address) {
+
+    this.weatherService.addressToLatLong(address).subscribe(
+      data => {
+
+        if (data['results'].length > 0) {
+
+          let lat: number = data['results'][0]['location']['lat'];
+          let long: number = data['results'][0]['location']['lng'];
+          this.weatherService.lat_long.next([lat, long])
+        }
+
+      },
+      error => console.error(error)
+    );
+
+  }
+
+  grid(coords: number[]) {
+
+    return this.weatherService.gridByLatLong(coords[0], coords[1]);
+  }
+
+  forecast(station: string, coords: number[]) {
+    return this.weatherService.forecast(station, coords[0], coords[1]);
+  }
 }
