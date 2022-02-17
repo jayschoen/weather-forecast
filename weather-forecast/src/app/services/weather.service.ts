e1c53619cab0f600469fc15a1146d951f3bdf61f
Jay Schoen <jay.r.schoen@gmail.com>
2022-02-17 16:58:23 -0500

    add get requests to convert address to forecast

--- weather-forecast/src/app/services/weather.service.ts
+++ weather-forecast/src/app/services/weather.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import { BehaviorSubject, Subject } from 'rxjs';
+
+import { Address } from '../address';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  lat_long = new Subject<number[]>();
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  addressToLatLong(address: Address) {
+
+    let url = environment.geocodeio_latlong
+ 
+    let addressString = `${address.street} ${address.city} ${address.state} ${address.zip}`;
+
+    let apiKey = environment.geocodio_api_key;
+
+    const params = new HttpParams()
+      .set('q', addressString)
+      .set('api_key', apiKey);
+
+    return this.httpClient.get( url, {params} );
+
+  }
+
+  gridByLatLong(lat: number, long: number) {
+    
+    let url = environment.nws_grid + `${lat},${long}/`
+
+    return this.httpClient.get( url );
+
+  }
+
+  forecast(station: string, gridX: number, gridY: number) {
+
+    let url = environment.nws_forecast + `${station}/${gridX},${gridY}/forecast`
+
+    return this.httpClient.get( url );
+  }
+
 }
